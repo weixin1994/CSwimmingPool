3fc32db344e820cd8c3485cb32a43130548fe46a
WeiXin <690521211@qq.com>
2018-03-14 14:07:41 +0800

    fixed day04

--- CPool_Day04/my_getnbr.c
+++ CPool_Day04/my_getnbr.c
@@ -35,6 +35,8 @@ int my_getnbr(char const *str)
 	int flag_isnum = 0;
 	while(i < len)
 	{
+		if(str[i] >= 'a' && str[i] <= 'z')
+			break;
 		if(str[i] == '+')
 			count_plus++;
 		if(str[i] == '-')
@@ -45,11 +47,11 @@ int my_getnbr(char const *str)
 			if(str[i+1] < '0' || str[i+1] > '9')
 				flag_isnum = 1;
 		}
+		
 		i++;
 		if(flag_isnum == 1)
 			break;
 	}
-	//printf("%d,%d,%d,%d",count_plus,count_minus,count_num,i);
 	char array[count_num-1];
 	int count = 0;
 	count = count_num - 1;
@@ -64,7 +66,6 @@ int my_getnbr(char const *str)
 		j++;
 	}
 	array[count_num] = '\0'; 
-	//printf("%s",array);
 	char *array2 = my_evil_str(array);
 	printf("%s",array2);
 		
@@ -103,16 +104,6 @@ int my_getnbr(char const *str)
 		if(count_minus % 2 == 1)
 			s = s * -1;
 	}
-	printf("flag_bey=%d",flag_bey);
-		
-	printf("s=%d",s);
 	return s;
 }
 
-int main()
-{
-	//char *str = "+++----+--444343345++3+--";
-	char *str = "-a++42";
-	my_getnbr(str);
-	return 0;
-}
