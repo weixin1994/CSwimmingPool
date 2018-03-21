08faf93113784ba9a3b8d33ea1aac86a381f46ea
WeiXin <690521211@qq.com>
2018-03-21 13:08:18 +0800

    can print minus max

--- CPool_Day03/ex_07/my_put_nbr.c
+++ CPool_Day03/ex_07/my_put_nbr.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+int my_strlen(char const *str);
+int my_putstring(char *str)
+{
+	return write(1,str,my_strlen(str));	
+}
 int my_transform(int num)
 {
 	if(num >= 10)
@@ -17,14 +22,19 @@ int my_transform(int num)
 
 int my_put_nbr(int nb)
 {
-	if(nb >= 0)
-		my_transform(nb);
+	if(nb == -2147483648)
+		my_putstring("-2147483648");
 	else
 	{
-		char minus = '-';
-		nb = nb * -1;
-		write(1,&minus,1);
-		my_transform(nb);
+		if(nb >= 0)
+			my_transform(nb);
+		else
+		{
+			char minus = '-';
+			nb = nb * -1;
+			write(1,&minus,1);
+			my_transform(nb);
+		}
 	}
 	return 0;
 }
