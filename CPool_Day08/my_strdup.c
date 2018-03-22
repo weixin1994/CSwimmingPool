8b77cec56fc74be3a18356579bfa1dae83f2df15
WeiXin <690521211@qq.com>
2018-03-22 20:39:34 +0800

    fix bug

--- CPool_Day08/my_strdup.c
+++ CPool_Day08/my_strdup.c
@@ -5,7 +5,7 @@ int my_strlen(char const *str);
 char *my_strdup(char const *src)
 {
 	int i = 0;
-	char *dest = malloc(my_strlen(src));
+	char *dest = malloc(my_strlen(src) + 1);
 	while (src[i] != '\0') 
 	{
 		dest[i] = src[i];
